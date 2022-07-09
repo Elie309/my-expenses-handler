ec9d2004188a02a3bcbbf3f6d619dfb55b8cb49f
elie309 <elie.saade432@gmail.com>
2022-07-09 21:05:02 +0300

    Changing colors and fonts

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -3,21 +3,25 @@ export const CategoryColor = {
     Expenses: "rgb(227,38,54)",
     Income: "#29AB87",
     Investment: "yellow",
+    Other: "gray"
 
 }
 
 export const fontColors = {
-    dark: "#323236",
-    primary: "#FFCC33",
-    secondary: "#094074",
+    primary: "#403F4C",
+    secondary: "#3185FC",
     lightGrey: "#8e8e8e",
+    white: "white",
+    dark: "#4D4D4D",
     red: "#DF3737"
 }
 
 export const backgroundColors = {
-    primary: "#094074",
-    secondary: "#FFCC33",
-    lightGrey: "#E5E5E5",
+    primary: "#49A078",
+    secondary: "#403F4C",
+    third: "#3185FC",
+    lightGrey: "#DEE5E5",
+
     darkGrey: "#cccccc",
-    white: "#FFFFFF",
+    white: "white",
 }
---
+++ src/utils/Font.ts
@@ -0,0 +1,6 @@
+export const fontSize = {
+    headerTittle: 25,
+    headerSubtitle: 20,
+    text: 12,
+    small: 9,
+}
